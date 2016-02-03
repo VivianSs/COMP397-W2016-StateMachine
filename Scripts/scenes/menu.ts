6319783e4fd15613abed31cb1bef150d68dfe379
VivianSs <vivianss0301@gmail.com>
2016-02-03 02:46:02 -0500

    updated menu scene

--- Scripts/scenes/menu.ts
+++ Scripts/scenes/menu.ts
@@ -23,7 +23,7 @@ module scenes {
 
             this.addChild(this._helloLabel); //this---- scene object
            
-            this._startButton = new objects.Button("StartButon",
+            this._startButton = new objects.Button("StartButton",
                 config.Screen.CENTER_X, config.Screen.CENTER_Y + 60)
 
             this.addChild(this._startButton);
